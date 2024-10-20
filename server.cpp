ea48a4fa2191cdf07bdc1549fc176cb12f516447
adamgj3030 <adamgj3030@gmail.com>
2024-10-20 15:51:58 -0500

    added ability to send structs, and added examples to refer to

--- server.cpp
+++ server.cpp
@@ -1,155 +1,69 @@
+#include <unistd.h>
 #include <iostream>
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <map>
-#include <cmath>
-#include <algorithm>
-#include <signal.h>
-#include <sys/wait.h>
+#include <netdb.h>
+// added this library manually
+#include <cstring> 
 
-struct CharCode {
-    char character;
-    int freq;
-    std::string code;
+struct symbol
+{
+    char name;
+    int frequency;
+    double probability;
+    char code[10];
 };
 
-// Function to handle zombie processes
-void fireman(int) {
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-}
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    std::string buffer;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-// Function to convert probability to binary
-std::string decimalToBinary(float decimal, int precision) {
-    std::string binary = "";
-    while (decimal > 0 && precision > 0) {
-        decimal *= 2;
-        if (decimal >= 1) {
-            binary += "1";
-            decimal -= 1;
-        } else {
-            binary += "0";
-        }
-        --precision;
+    if (argc != 3) 
+    {
+       std::cerr << "usage " << argv[0] << " hostname port" << std::endl;
+       exit(0);
     }
-    return binary;
-}
-
-// Shannon encoding function
-std::string shannonEncode(const std::string &message, std::vector<CharCode> &alphabet) {
-    std::map<char, int> frequencyMap;
-    for (char c : message) {
-        frequencyMap[c]++;
-    }
-
-    for (auto &entry : frequencyMap) {
-        CharCode cc;
-        cc.character = entry.first;
-        cc.freq = entry.second;
-        alphabet.push_back(cc);
-    }
-
-    std::sort(alphabet.begin(), alphabet.end(), [](CharCode a, CharCode b) {
-        return a.freq > b.freq;
-    });
-
-    float cumulativeProbability = 0.0;
-    for (auto &cc : alphabet) {
-        float probability = static_cast<float>(cc.freq) / message.size();
-        int precision = ceil(log2(1.0 / probability));
-        cc.code = decimalToBinary(cumulativeProbability, precision);
-        cumulativeProbability += probability;
-    }
-
-    std::string encodedMessage = "";
-    for (char c : message) {
-        for (const auto &cc : alphabet) {
-            if (cc.character == c) {
-                encodedMessage += cc.code;
-                break;
-            }
-        }
-    }
-    return encodedMessage;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./server <port_no>" << std::endl;
-        return 1;
-    }
-
-    int portno = atoi(argv[1]);
-    int sockfd, newsockfd;
-    struct sockaddr_in serv_addr, cli_addr;
-    socklen_t clilen;
-
-    signal(SIGCHLD, fireman); // Prevent zombie processes
-
+    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Error opening socket" << std::endl;
-        return 1;
+    if (sockfd < 0) 
+    {
+        std::cerr << "ERROR opening socket" << std::endl;
+        exit(0);
     }
-
-    bzero((char *)&serv_addr, sizeof(serv_addr));
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        std::cerr << "ERROR, no such host" << std::endl;
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
     serv_addr.sin_port = htons(portno);
-
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Error binding socket" << std::endl;
-        return 1;
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    {
+        std::cerr << "ERROR connecting" << std::endl;
+        exit(0);
     }
 
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
+    // added function to send a struct
+    symbol s;
+    s.name = 'a';
+    s.frequency = 1;
+    s.probability = 1;
+    strcpy(s.code, "adam\0");
 
-    while (true) {
-        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-        if (newsockfd < 0) {
-            std::cerr << "Error accepting connection" << std::endl;
-            continue;
-        }
-
-        if (fork() == 0) {
-            close(sockfd);
-
-            int msgSize;
-            read(newsockfd, &msgSize, sizeof(int));
-            char *message = new char[msgSize + 1];
-            read(newsockfd, message, msgSize);
-            message[msgSize] = '\0';
-
-            std::string inputMessage = message;
-            std::vector<CharCode> alphabet;
-            std::string encodedMessage = shannonEncode(inputMessage, alphabet);
-
-            // Send alphabet
-            int alphabetSize = alphabet.size();
-            write(newsockfd, &alphabetSize, sizeof(int));
-            for (const auto &cc : alphabet) {
-                write(newsockfd, &cc.character, sizeof(char));
-                write(newsockfd, &cc.freq, sizeof(int));
-                int codeSize = cc.code.size();
-                write(newsockfd, &codeSize, sizeof(int));
-                write(newsockfd, cc.code.c_str(), codeSize);
-            }
-
-            // Send encoded message
-            int encodedSize = encodedMessage.size();
-            write(newsockfd, &encodedSize, sizeof(int));
-            write(newsockfd, encodedMessage.c_str(), encodedSize);
-
-            delete[] message;
-            close(newsockfd);
-            exit(0);
-        }
-        close(newsockfd);
+    n = write(sockfd,&s,sizeof(s));
+    if (n < 0) 
+    {
+        std::cerr << "ERROR writing to socket" << std::endl;
+        exit(0);
     }
 
     close(sockfd);
